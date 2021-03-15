7ecb76bfb07f912d1ff95a135e53db7ad2653701
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-03-15 10:03:49 +0200

    pradejom palyginima

--- js/operatoriai.js
+++ js/operatoriai.js
@@ -16,9 +16,42 @@ res = x ** y; console.log('x ** y:', res); // Kelimas laipsniu
 res = x ** (1 / y); console.log('x ** (1 / y):', res); // Saknis
 
 res = x + y / 2 + 3; console.log('x + y / 2 + 3:', res); // sudetingesins skaiciavimas
-console.log('--- ---- ---- -----');
+console.log('--- ---- ---- ----- ---- ---- -----');
 
 // vieno nario operatoriai 
+let c = 5;
+console.log('-- Vieno nario operatoriai');
+console.log('c =', c);
+// console.log('c++', c++); // Ivygdyti komanda ir tada padidinti vienetu
+// console.log('++c', ++c); // Pirmiausia padidinam tada ivygdom komanda
+// console.log('c--', c--); // Ivygdyti komanda ir tada pamazinti vienetu
+// console.log('--c', --c); // Ivygdyti komanda ir tada pamazinti vienetu
+
+c += 2; console.log('c += 2; c:', c); // Prie esamos c reiksmes pridedam 2 ir irasom atagl i ta pati kintamaji c
+c -= 2; console.log('c -= 2; c:', c); // Is esamos c reiksmes atimam 2 ir irasom atagl i ta pati kintamaji c
+c *= 2; console.log('c *= 2; c:', c); // esama c reiksme padauginam is 2 ir irasom atagl i ta pati kintamaji c
+c /= 2; console.log('c /= 2; c:', c); // esama c reiksme padalinam is 2 ir irasom atagl i ta pati kintamaji c
+c %= 2; console.log('c %= 2; c:', c); // Skaiciuoti esamos c vertes liekana is dvieju ir irasom atagl i ta pati kintamaji c
+console.log('--- ---- ---- ----- ---- ---- -----');
 
 
+
+ 
+// -- Loginiai operatoriai
+let lijoUzvakar = true;
+let lijoVakar = false;
+let uzvakarTemp = '5';
+let vakarTemp = 5;
+let siandienTemp = 3;
+
+console.log('-- Loginiai operatoriai');
+console.log('---- lygybes ir nelygybes, salyginiai palyginimai');
+console.log(
+    'lijoUzvakar:', lijoUzvakar, 
+    'lijoVakar:', lijoVakar, 
+    'uzvakarTemp:', uzvakarTemp,
+    'vakarTemp:', vakarTemp,
+    'siandienTemp:', siandienTemp
+);
+console.log('vakarTemp > siandienTemp; ', vakarTemp > siandienTemp);
 
