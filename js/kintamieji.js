06e6f3f797a5b6db41ec6ad00edcdfa1b8bc0643
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-03-15 09:11:17 +0200

    kintamieji done

---
+++ js/kintamieji.js
@@ -0,0 +1,32 @@
+// console.log('app.js');
+
+// kintamuju sukurimas
+let x = 15;
+let y = 17;
+
+console.log('x =', x, 'y =', y);
+
+// pakeisti x reiksme i 23
+x = 23;
+
+console.log('--- pakeitem x reiksme ---');
+console.log('x =', x, 'y =', y);
+
+
+// console.log(5 + 10);
+
+let name = "Bob";
+let birthday = 'geguzes 17d';
+
+// hardcoded sentence
+console.log('James yra gimes geguzes 12d');
+
+// panaudoti kintamuosius sakinio atspausdinimui
+console.log( name + ' yra gimes ' + birthday);
+
+
+
+
+
+
+
