f611a95f777bdba109c27261b17e1d47a72dc8f5
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-03-15 09:31:52 +0200

    paprasti operatoriai

---
+++ js/operatoriai.js
@@ -0,0 +1,24 @@
+console.log('operatoriai.js');
+
+// Pradiniai kinamieji
+let x = 16;
+let y = 4;
+let res;
+console.log('-- Dvieju nariu operatoriai');
+console.log('x:', x, 'y:', y);
+
+res = x + y; console.log('x + y:', res); // Sudetis
+res = x - y; console.log('x - y:', res); // Atimtis
+res = x * y; console.log('x * y:', res); // Daygyba
+res = x / y; console.log('x / y:', res); // Dalyba
+res = x % y; console.log('x % y:', res); // Liekana
+res = x ** y; console.log('x ** y:', res); // Kelimas laipsniu
+res = x ** (1 / y); console.log('x ** (1 / y):', res); // Saknis
+
+res = x + y / 2 + 3; console.log('x + y / 2 + 3:', res); // sudetingesins skaiciavimas
+console.log('--- ---- ---- -----');
+
+// vieno nario operatoriai 
+
+
+
