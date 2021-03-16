6b79c196d5b574204079122852be5be605edd275
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-03-16 15:12:50 +0200

    for ciklas generuoja li elementus

--- js/ciklai.js
+++ js/ciklai.js
@@ -1,4 +1,14 @@
 console.log('ciklai.js');
+/*  Dažnai programavime reikia kartoti kodą (su mažom variacijom) daug kartų:
+  - rikiavimui, filtravimui, atrinkimui, validacijai, ir kitiems algoritmams.
+  Tokiam tikslui mums praverčia progravaimo įrankis - ciklas.
+  Ciklai yra kelių rūšių, bet pasižymi tais pačias kriterijais. 
+  Ciklas privalo turėti tokius 3 logines sudedamąsias dalis:
+  -- tęstinumo/baigtinė salyga -> Nurodo ar ciklas tęsis, ar užsibaigs/
+  -- pradinis/darbinis kitamasis -> Kintamasis kuris privalo dalyvauti tęstinumo/baigties salygoje.
+  -- žingsnis -> tai pradinio/darbinio kintamojo kitimas, jog jis įtakotų tęstinumo salygą, ir artėtų link baigtinės salygos.
+*/
+
 
 console.log('-------------WHILE CIKLAS-------------');
 
@@ -32,6 +42,49 @@ for (let i = 0; i < 10; i++) {
     // loop body
     console.log('for ciklas works', i);
 }
+
+
+// for ciklo pagalba atspausdinti console skaicius nuo 0 iki 56
+// for ciklo pagalba atspausdinti console skaicius nuo 34 iki 100
+
+for(let i = 34; i < 100 ; i++) {
+    // console.log(i);
+}
+// for ciklo pagalba atspausdinti console skaicius nuo 99 iki 23
+for(let i = 99; i >= 23 ; i--) {
+    // console.log(i);
+}
+// for ciklo pagalba atspausdinti console skaicius nuo 0 iki 72 kas trecia sk
+for (let i = 0; i <= 72; i++) {
+    if (i % 3 !== 0) {
+        continue;
+    } else {
+        console.log(i);
+    }
+    
+}
+
+// for ciklo pagalba li elemetus su pavadinimu  10
+// <li>elementas 0 </li>
+// <li>elementas 1 </li>
+// <li>elementas 2 </li>
+console.clear();
+let sarasas = '';
+for (let i = 0; i <= 10; i++) {
+    // console.log(i);
+    // console.log(`<li>elementas ${i} </li>`);
+    sarasas = sarasas + `<li>elementas ${i} </li>`;
+}
+console.log(sarasas);
+
+
+const appDivas = document.getElementById('app');
+
+console.log(appDivas);
+
+appDivas.innerHTML = sarasas;
+
+
 
 
 
