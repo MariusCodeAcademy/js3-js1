0e7ecbac349b4c7f7c330963fad622f9fe5a55c5
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-03-16 09:02:40 +0200

    masyvu metodai baigem

--- js/masyvuMetodai.js
+++ js/masyvuMetodai.js
@@ -1,7 +1,7 @@
 console.log('masyvu metodai');
 
 // index        0         1        2        3     
-const things = ['apple', 'sister', 'hammer', 'sun'];
+let things = ['apple', 'sister', 'hammer', 'sun'];
 
 console.log(things);
 
@@ -39,3 +39,46 @@ console.log(things);
 things.splice(1, 2, 'sky', 'moon');
 console.log('--- array splice');
 console.log(things);
+
+// array splice naudojamas istrinti kazkuriam masyvo elementui
+// istrinti the moon kurio index yra 2
+
+let spliced = things.splice(2, 1);
+console.log('--- array splice istrinti moon');
+console.log(things, 'spliced result:', spliced);
+
+// arr.slice()
+let sliced = things.slice(-2)
+console.log('--- array slice -2');
+console.log(things);
+console.log('sliced:', sliced);
+
+// lets add few more elements to array blue, green, yellow
+// things.push('blue', 'green', 'yellow');
+// things = things.concat('blue', 'green', 'yellow');
+things.splice(things.length, 0, 'blue', 'green', 'yellow')
+console.log('--- array add blue, green, yellow');
+console.log(things);
+
+console.clear()
+// naudojant slice gauti nauja masyva su nariais nuo sun iki green
+let slicedThings = things.slice(2, 5);
+console.log('--- array slice nuo sun iki green ');
+console.log(things);
+console.log('sliced:', slicedThings);
+
+// naudojant slice gauti nauja masyva nuo sun iki galo
+slicedThings = things.slice(2);
+console.log('--- array slice nuo sun iki galo ');
+console.log(things);
+console.log('sliced:', slicedThings);
+
+
+// masyvu sujungimas su concat things sujungti su concat naujo masyvo pavidalu
+
+let sujugtas = slicedThings.concat(things)
+console.log(sujugtas);
+console.log(things);
+
+
+
