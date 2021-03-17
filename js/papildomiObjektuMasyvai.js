a82338479883ba51cd20de65d56e914066b39d92
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-03-17 13:46:01 +0200

    papildomiObjektuMasyvai

---
+++ js/papildomiObjektuMasyvai.js
@@ -0,0 +1,44 @@
+const peopleArray = [
+  {
+    name: "Serbentautas",
+    surname: "Bordiūras",
+    age: 31,
+  },
+  {
+    name: "Monitorė",
+    surname: "Vaizdinienė",
+    age: 28,
+  },
+  {
+    name: "Sandra",
+    surname: "Barantaitė",
+    age: 32,
+  },
+  {
+    name: "Velinas",
+    surname: "Dviratis",
+    age: 25,
+  },
+  {
+    name: "Vajetauskas",
+    surname: "Ištiktenis",
+    age: 11,
+  },
+  {
+    name: "Kęstas",
+    surname: "Paskęstas",
+    age: 12,
+  },
+  {
+    name: "Marytė",
+    surname: "Grėblytė",
+    age: 44,
+  },
+];
+
+// 1. Atspausdintikie consoleje visus vardus,
+// 2. sukurkite nauja masyva ir uzpildykite ji pavardemis,
+// 3. sukurkite funkcija, kuri prideda prie kiekvieno amziaus texta ' metai'
+// 4. sukurkite funkcija kuri ima peopleArray kaip argumenta ir grazina li elementus su 'vardas pavarde yra metai amziaus' reiksmemis
+// 5 . Atrinkite naują žmonių masyvą, kuriame būtų tik jaunesni nei 30 metų žmonės. Atspausdinkite.
+// 6. Suskaičiuokite visų žmonių esančių 'peopleArray' amžiaus vidurkį. Atspausdinkite.
