1bbb2c1bfe7be8aa04662da9aed7494ea1c5fe7a
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-03-17 12:08:38 +0200

    uzduotis su array obj

--- js/masyvuObjektai.js
+++ js/masyvuObjektai.js
@@ -1,3 +1,46 @@
+console.log("___ masyvu masyvai ___");
+
+let masyvas = [
+  //0  1  2
+  [1, 2, 3], // 0
+  [4, 5, 6], // 1
+  [7, 8, [10, 11, 12]], // 2
+];
+
+console.log(masyvas[0][1]);
+
+// gauti 9
+console.log(masyvas[2][2]);
+
+console.log(masyvas[2][2][2]);
+
+console.log("___ objektu masyvai ___");
+
+let persons = [
+  { name: "Bob", age: 23, town: "Vilnius", isStudent: true }, // 0
+  { name: "Jane", age: 25, town: "London", isStudent: false }, // 1
+  { name: "James", age: 20, town: "Vilnius", isStudent: true }, // 2
+  { name: "Mary", age: 21, town: "Tokyo", isStudent: true }, // 3
+];
+
+console.log("pirmas objektas", persons[0]);
+console.log("antro asmens vardas:", persons[1].name);
+
+// atspausdinti consoleje vardu zmoniu kurie yra studentai
+
+// 1. sukti cikla per masyva
+// 3. jei tai yra studentas tai mes norim atspausdinti varda.
+console.clear();
+for (let i = 0; i < persons.length; i++) {
+  // console.log(persons[i]);
+  // 2 ciklo metu mes turim patikrinti ar asmuo yra studentas
+  if (persons[i].isStudent === true) {
+    console.log(persons[i].name, "is a student ");
+  }
+}
+
+// atspausdinti studentu vardus ir miestu kuriu amziu yra daugiau nei 22 metai
+
 const blogs = [
   {
     title: "my first blog",
@@ -12,7 +55,7 @@ const blogs = [
   },
 ];
 
-console.log(blogs[1].body);
+// console.log(blogs[1].body);
 let blog1 = {
   title: "my first blog",
   author: "john doe",
