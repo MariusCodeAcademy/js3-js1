770667218eb5c5db3f268f68131b0722ada321ee
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-03-17 15:32:13 +0200

    arr.join()

--- js/pratimai1.js
+++ js/pratimai1.js
@@ -13,14 +13,30 @@ function getFirstEl() {
   return numbers[0];
 }
 
-let firstEl = getFirstEl();
+// let firstEl = getFirstEl();
 
-console.log("first el", firstEl);
-console.log("numbers:", numbers);
+// console.log("first el", firstEl);
+// console.log("numbers:", numbers);
 
 // 2. Parašykite funkciją, kuri pašalina pirmą masyvo elementą ir jį grąžina.
+function cutMeFirstEl() {
+  return numbers2.shift();
+}
+// let firsElAfterCut = cutMeFirstEl();
+
+// console.log("firsElAfterCut", firsElAfterCut);
+// console.log("numbers:", numbers);
 
 // 3. Parašykite funkciją, kuri grąžina paskutinį masyvo elementą.
+function giveMeLastEl(arr) {
+  //   let lastEl = arr.slice(-1);
+  //   return lastEl[0];
+  let lastEl = arr[arr.length - 1];
+  return lastEl;
+}
+
+// let lastEl = giveMeLastEl(numbers);
+// console.log("lastEl", lastEl);
 
 // 4. Parašykite funkciją, kuri pašalina paskutinį masyvo elementą ir jį grąžina.
 
@@ -29,6 +45,12 @@ console.log("numbers:", numbers);
 // 6. Parašykite funkciją, kuri atspausdina paskutinio masyvo elemento indeksą
 
 // 7. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento indeksus eilutėmis. console.log()
+function printIndexesPlease(arr) {
+  for (let index = 0; index < arr.length; index += 1) {
+    console.log(index);
+  }
+}
+// printIndexesPlease(numbers);
 
 // 8. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento indekso ir reikšmių poras eilutėmis, tokiu formatu:
 //  [0] => reikšmė
@@ -37,6 +59,22 @@ console.log("numbers:", numbers);
 //  ..............
 
 // 9. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento reiksmes vienoje eilutėje:  -111 2 -9 48
+function printArrayValuesLine(arr) {
+  //   let resultString = "";
+  //   for (let index = 0; index < arr.length; index += 1) {
+  //     resultString = resultString + arr[index] + " ";
+  //   }
+  //   console.log(resultString);
+
+  console.log(arr.join(" "));
+}
+printArrayValuesLine(numbers);
+
+// sukti cikla
+// kiekviena reiksme irasyti atskirta tarpu
+// atspausdinti visa rezulta
 
 // 10. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento indekso ir reikšmių poras vienoje eilutėje, tokiu formatu:
 // [0]=>17 [1]=>8 [2]=>88 ...
+
+console.log("numbers after operation:", numbers);
