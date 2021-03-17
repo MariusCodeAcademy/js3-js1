930402641eebf8f0773da6a446ad6e3e5cdeea2d
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-03-17 11:41:05 +0200

    baigem su uzd

--- js/cikluUzdaviniai2.js
+++ js/cikluUzdaviniai2.js
@@ -1,3 +1,4 @@
+"use strict";
 console.log("------------- FOR CIKLAS Dirba su masyvais -------------");
 
 // in           0    1   2   ....
@@ -6,7 +7,7 @@ let numbers1 = [1, -43, -40, -14, -37, -8, 41, 27, -36, 36];
 let sakinys = "Labas vakaras zmones js yra ne taip ir sunku";
 
 // 1. parasyti funkcija kuri grazina nauja masyva:
-//    a. tik su teigiamom reiksmem
+//    a. tik su teigiamom reiksmem / [ 10, 8, 49, 47 ]
 //    b. tik su neigiamom reiksmem
 //    c. paverte neigiamsu skaicius i teigiamus
 //    d. grazinti is antros puses
@@ -17,31 +18,60 @@ let sakinys = "Labas vakaras zmones js yra ne taip ir sunku";
 //  extra funkcija kuri grazini sakini is priesingos puses;
 
 // extra extra grazinti masyva us apsuktom neigiamom sekom
+// console.log(numbers);
+
+// a tik su teigiamom reiksmem / [ 10, 8, 49, 47 ]
+
+function getPositiveArr(arr) {
+  // sukurti vieta kur issaugosim teigiamas reiksmes
+  let postiveArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    // ar einamasis el yra daugiau uz 0
+    if (arr[i] > 0) {
+      postiveArr.push(arr[i]);
+    }
+  }
+  return postiveArr;
+}
+
+console.log(getPositiveArr(numbers));
 console.log(numbers);
 
-const blogs = [
-  {
-    title: "my first blog",
-    author: "john doe",
-    body:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ea facilis dignissimos inventore at doloremque sunt sint numquam nemo quibusdam, cupiditate tenetur reiciendis cum commodi odit laudantium omnis officiis cumque?",
-  },
-  {
-    title: "my second blog",
-    author: "john doe",
-    body: "Lorem que?",
-  },
-];
+function makeAllPositiveArr(arr) {
+  let allPositive = [];
+  let tempValue;
+  for (let i = 0; i < arr.length; i++) {
+    // norim paversti neigiamus teigiamais
+    if (arr[i] > 0) {
+      allPositive.push(arr[i]);
+    } else {
+      // paverciam skaiciu i teigiama
+      tempValue = arr[i] * -1;
+      allPositive.push(tempValue);
+    }
+  }
+  console.log("all positive", allPositive);
+  return allPositive;
+}
 
-console.log(blogs[1].body);
-let blog1 = {
-  title: "my first blog",
-  author: "john doe",
-  body:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ea facilis dignissimos inventore at doloremque sunt sint numquam nemo quibusdam, cupiditate tenetur reiciendis cum commodi odit laudantium omnis officiis cumque?",
-};
-let blog2 = {
-  title: "my second blog",
-  author: "john doe",
-  body: "Lorem que?",
-};
+makeAllPositiveArr(numbers);
+
+console.clear();
+console.log(numbers);
+
+console.log("----Is antros puses----");
+
+function reverseArr(arr) {
+  let reversedArr = [];
+  // pradine index reiksme yra masyvo ilgis - 1
+  for (let index = arr.length - 1; index >= 0; index--) {
+    reversedArr.push(arr[index]);
+  }
+  console.log("reversedArr:", reversedArr);
+
+  return reversedArr;
+}
+
+reverseArr(numbers);
+console.log(numbers);
---
+++ js/masyvuObjektai.js
@@ -0,0 +1,26 @@
+const blogs = [
+  {
+    title: "my first blog",
+    author: "john doe",
+    body:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ea facilis dignissimos inventore at doloremque sunt sint numquam nemo quibusdam, cupiditate tenetur reiciendis cum commodi odit laudantium omnis officiis cumque?",
+  },
+  {
+    title: "my second blog",
+    author: "john doe",
+    body: "Lorem que?",
+  },
+];
+
+console.log(blogs[1].body);
+let blog1 = {
+  title: "my first blog",
+  author: "john doe",
+  body:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ea facilis dignissimos inventore at doloremque sunt sint numquam nemo quibusdam, cupiditate tenetur reiciendis cum commodi odit laudantium omnis officiis cumque?",
+};
+let blog2 = {
+  title: "my second blog",
+  author: "john doe",
+  body: "Lorem que?",
+};
