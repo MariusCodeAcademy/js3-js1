5d16aeb3c4f20c263748e1f9cf74b0a225b32327
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-03-22 09:00:02 +0200

    terenary operator + do while

--- js/salyginiaiSakSuFunkcijom.js
+++ js/salyginiaiSakSuFunkcijom.js
@@ -1,16 +1,24 @@
 "use strict";
 console.log("Salyginiai sakiniai su funkcijom");
 
+const msgInput = "Kiek jums metu? Iveskite skaiciu.";
+const msgError = "Ivedete ne skaiciu.";
+
 // si funkcija grazina true arba false priklausomai ar value yra skaicius.
 function checkIfValueIsNumeric(value) {
   // atvejis jei vartotojas ivede '' / tiesiog paspaude ok;
+  if (value === "") {
+    console.log("Jus nieko neivedete, prasom ivesti skaiciu.");
+    return false;
+  }
 
-  //   debugger;
+  // atvejis kai turim galimai skaitine verte arba ne
   value = Number(value);
   // patikrinti ar mes gaunam skaiciu? ar gaunam NaN
-  if (typeof value !== "number" || isNaN(value)) {
-    console.warn("ivedete ne skaiciu");
-    return;
+  if (isNaN(value)) {
+    return false;
+  } else {
+    return true;
   }
 }
 
@@ -21,33 +29,27 @@ function userAgeResponces() {
   // ir pagal gauta reiksme atsakyti atitinkama atsakyma
   let age;
 
-  age = prompt("Kiek jums metu?");
+  age = prompt(msgInput);
 
+  // atskiras atvejis kur do while loop yra tinkamesnis naudoti uz kitus
+  //   do {
+  //     age = prompt(age === null ? msgInput : msgError);
+  //   } while (isNaN(age) || age < 0);
+
+  // atvejis kai paspausta cancel
   if (age === null) {
     console.warn("Jus nutraukete ivedima");
     return;
   }
 
-  console.log("age", age);
-
-  if (checkIfValueIsNumeric(age)) {
-    // tai yra skaicius
-  } else {
+  if (!checkIfValueIsNumeric(age)) {
     // tai nera skaicius
+    console.error("prasom ivesti skaiciu");
+    return;
   }
 
   // patikrinti ar ivesta skaitine reikme yra daugiau uz 0
   // arba maziau uz 150
-
-  // atskirais sakiniais
-  //   if (age < 0) {
-  //     console.error("amzius negali buti maziau uz 0");
-  //     return;
-  //   }
-  //   if (age > 150) {
-  //     console.error("amzius negali buti daugiau uz 150");
-  //     return;
-  //   }
 
   // bendra salyga su loginiu OR
   if (age < 0 || age > 150) {
@@ -75,3 +77,29 @@ function userAgeResponces() {
   // atsakymas "nedek daiktu i burna"
 }
 userAgeResponces();
+
+function terenaryOperator(value) {
+  const msgDaugiau = "daugiau uz 54";
+  const msgMaziau = "maziau uz 54";
+  let msg = "";
+  // jei reiksme yra daugiau uz 54
+  // lg daugiau uz 54
+
+  // lg maziau uz 54
+
+  //   if (value > 54) {
+  //     console.log(msgDaugiau);
+  //   } else {
+  //     console.log(msgMaziau);
+  //   }
+
+  // terenary operator - vienos eilute if else statement
+  // salyga ? jei true : jei false;
+  //   value > 54 ? console.log(msgDaugiau) : console.log(msgMaziau);
+
+  msg = value > 54 ? msgDaugiau : msgMaziau;
+
+  console.log(msg);
+}
+
+// terenaryOperator(54);
