31d6b5d5e29f414d8563407ca9614243c0e0ff64
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-03-22 09:29:21 +0200

    tasks.js

---
+++ js/tasks.js
@@ -0,0 +1,99 @@
+"use strict";
+
+const people = [
+  {
+    //0
+    name: "Jonas",
+    surname: "Jonaitis",
+    sex: "male",
+    age: 26,
+    income: 1200,
+    married: false,
+    hasCar: false,
+  },
+  {
+    // 1
+    name: "Severija",
+    surname: "Piktutytė",
+    sex: "female",
+    age: 26,
+    income: 1300,
+    married: false,
+    hasCar: true,
+  },
+  {
+    name: "Valdas",
+    surname: "Vilktorinas",
+    sex: "male",
+    age: 16,
+    income: 0,
+    married: false,
+    hasCar: false,
+  },
+  {
+    name: "Virginijus",
+    surname: "Uostauskas",
+    sex: "male",
+    age: 32,
+    income: 2400,
+    married: true,
+    hasCar: true,
+  },
+  {
+    name: "Samanta",
+    surname: "Uostauskienė",
+    sex: "female",
+    age: 28,
+    income: 1200,
+    married: true,
+    hasCar: true,
+  },
+  {
+    name: "Janina",
+    surname: "Stalautinskienė",
+    sex: "female",
+    age: 72,
+    income: 364,
+    married: false,
+    hasCar: false,
+  },
+];
+
+// 1. Parašykite funkciją, kuri atspausdina visus žmones eilutėmis
+// function expression budu pasiimti masyva kaip argumenta.
+console.log(people[0]);
+
+// 2. Parašykite funkciją, kuri atspausdina visų žmonių vardus ir pavardes, atskirtus brūkšneliu, pvz.:
+// Serbentautas-Bordiūras
+
+// 3. Parašykite funkciją, kuri atspausdina visų žmonių vardus ir pavardes bei santuokos statusą, tokiu būdu
+//  Jonas Bikas: Vedęs
+//  Jonas Bikas: Nevedęs
+//  Samantas Kultaitytė: Ištekėjusi
+//  Samantas Kultaitytė: Neištekėjusi
+
+// 4. Parašykite funkcijas pagal 2 ir 3 užduotį, kurios vietoj to kad spausdintų, rezultatus grąžintų.
+// Tuomet rezultatai būtų atspausdinti naudojant funkciją aprašytą pirmu punktu.
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// // funkcijos delclaracija
+// function add5(value) {
+//   console.log(value + 5);
+// }
+
+// add5(10);
+
+// // function expression
+// const add10 = function (value) {
+//   console.log(value + 10);
+// };
+
+// add10(10);
